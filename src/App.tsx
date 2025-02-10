fa0d156b8abfbdc8acad759e778186fab4ec24f2
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-10 02:02:03 +0000

    Refactor App component with semantic main tag and export

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,18 @@ import { Navbar } from '../app/components/layout/Navbar';
 import LandingPage from '../app/pages/Landing';
 import PlaygroundPage from '../app/pages/Playground';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-900">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/playground" element={<PlaygroundPage />} />
-          {/* Add more routes here */}
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/playground" element={<PlaygroundPage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
 }
-
-export default App;
