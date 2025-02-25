09c0d02c20d1caa68c356a45b9432c50ca110eaa
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-25 06:52:07 +0000

    feat: enhance layout structure and update metadata for improved clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Navbar } from '@/components/layout/navbar'
-import { Footer } from '@/components/layout/Footer'
+import { Footer } from '@/components/layout/footer'
 import { ParticleConnectkit } from '@/context/connect'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-   title: 'Zenode',
-   description: 'Web3 Development Made Fun',
+   title: 'Zenode - Next-Gen Blockchain Development Platform',
+   description: 'Empowering developers with next-generation blockchain development tools and infrastructure.',
 }
 
 export default function RootLayout({
@@ -21,10 +21,14 @@ export default function RootLayout({
       <html lang="en">
          <body className={inter.className}>
             <ParticleConnectkit>
-               <Navbar />
-               {children}
+               <div className="min-h-screen flex flex-col">
+                  <Navbar />
+                  <main className="flex-grow">
+                     {children}
+                  </main>
+                  <Footer />
+               </div>
             </ParticleConnectkit>
-            <Footer />
          </body>
       </html>
    )
